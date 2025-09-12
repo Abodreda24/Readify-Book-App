a25774e3d4a43cf95b61d903c3e4cf271c1113e3
Abodreda24 <abodreda5556@gmail.com>
2025-09-12 16:32:27 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:readify/features/splash/prescntion/views/splashview.dart';
+
+void main() {
+  runApp(const Readify
+());
+}
+
+class Readify
+ extends StatelessWidget {
+  const Readify
+({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:  SplashView(),
+    );
+  }
+}
+
+ 
